45c22cade7720ff605634d308d0a36be4960bcf9
ysk <otawa.yusuke@gmail.com>
2019-04-28 16:58:40 +0900

    add echo and make an endpoint on /

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
-import "fmt"
 
-func main(){
-    fmt.Printf("Hello, world\n")
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/", func(c echo.Context) error { //c をいじって Request, Responseを色々する
+		return c.String(http.StatusOK, "root")
+	})
+
+	// サーバー起動
+	e.Start(":8100") //ポート番号指定してね
 }
