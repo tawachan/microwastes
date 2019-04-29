0de920f8e21de237112ffe6c77a754b42fa92167
ysk <otawa.yusuke@gmail.com>
2019-04-29 22:02:46 +0900

    setup for kubernetes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@ VERSION=$(cat .version)
 HOST_NAME=asia.gcr.io
 PROJECT_ID=microwastes
 
-WASTES_NAME=$HOST_NAME/$PROJECT_ID/wastes:$VERSION
+WASTES_NAME=$HOST_NAME/$PROJECT_ID/main:$VERSION
 docker build . -f Dockerfile -t $WASTES_NAME
-docker push $WASTES_NAME
+gcloud docker -- push $WASTES_NAME
